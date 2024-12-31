1957d4c4ce8baa1378dde44e762f8df07a514255
wszx123 <108945398+wszx123@users.noreply.github.com>
2025-01-01 00:07:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -260,7 +260,7 @@ const index_html = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ws01导航</title>
+    <title>My Nav</title>
     <link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>💠</text></svg>">
     <style>
         :root {
@@ -570,7 +570,7 @@ const index_html = `<!DOCTYPE html>
 </head>
 <body>
     <div class="header">
-        <h1>ws01导航</h1>
+        <h1>My Nav</h1>
         <div class="header-right">
             <button class="theme-switch" onclick="toggleTheme()">
                 <svg class="sun-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
